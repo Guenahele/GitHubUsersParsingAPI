11b55ea390c7f9026a11bd44cc56688c1fdd247b
Guenahele <gbustin@yahoo.fr>
2022-09-27 12:16:58 +0200

    Initial Commit

---
+++ GitHubUsersParsingAPI/GitHubUsersParsingAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubUsersParsingAPIApp.swift
+//  GitHubUsersParsingAPI
+//
+//  Created by apprenant02 on 27/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct GitHubUsersParsingAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
